bc6524db319b7a8fef0d986f37491a320f421968
Mariene <mariene_w@hotmail.fr>
2017-12-14 22:52:08 +0100

    modif

--- extract_data.py
+++ extract_data.py
@@ -9,30 +9,47 @@ from __future__ import print_function
 import vcf
 import os
 
-path = os.path.join(os.getcwd(),'Data','global.pop.GATK.SNP.hard.filters.V3.phased_all.pop.recode.maf.05.recode.vcf')
+
+path = os.path.join(os.getcwd(),'Data','test.vcf')
 vcf_reader = vcf.Reader(open(path,'r'))
 
-record = vcf_reader.next() #iterator 
-# mais une boucle for ferait l'affaire 
-#for record in vcf_reader :
-#    print (record)
+#record = next(vcf_reader) #iterator
+# mais une boucle for ferait l'affaire
+for record in vcf_reader :
+    #print (record.samples)
+    #print ((record.samples)[0].sample)
+    print (record)
+    if len(record.ALT)>1 :
+         for i in vcf_reader.samples :
+             print (record.genotype(i))
+
+print (len(vcf_reader.samples))
+"""
 print (record)
 print (record.CHROM)
 print (record.POS)
-#print (record.ID)
+
 print (record.REF)
 print (record.ALT)
+
+
+print (record.is_snp)
+print (record.is_indel)
+print (record.is_transition)
+print (record.is_deletion)
+
+print (vcf_reader.samples)
+
+
+"""
 #print (record.QUAL)
 #print (record.FILTER)
 #print (record.INFO)
 #print (record.FORMAT)
 #print (record.samples)
 #print (record.genotype)
+#print (record.ID)
 
-print (record.is_snp)
-print (record.is_indel)
-print (record.is_transition)
-print (record.is_deletion)
-print (vcf_reader.samples)
+
 
 
