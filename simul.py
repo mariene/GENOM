f0fdb0437db6cda842fb5e1951df9fd9f7c46e24
MarièneWan <mariene.wan@laposte.fr>
2018-01-15 18:58:49 +0100

    ajout process

--- simul.py
+++ simul.py
@@ -12,6 +12,7 @@ import glob
 import math
 from multiprocessing import Process,Queue,Pool,cpu_count
 import matplotlib.pyplot as plt
+
 def launch_simul(n,theta,rep) : 
     """Permet de lancer le logiciel SimulTrees
     
@@ -63,27 +64,24 @@ def launch_simul(n,theta,rep) :
     return path
     
 
-"""
+
 #Multiproc
 def lancement(s):
-    os.system(s)
-    filename = s.plit('>')[1]
+    os.system(s[1])
+    filename = s [0]
     f = open(filename,"r")
     contenu = f.readlines()
     f.close()
     recup = list()
-    #print (contenu)
-    
-    for k in range (len(contenu)-1) :
-        
 
+    for k in range (len(contenu)-1) :
         if contenu[k][:2] != '/*' and contenu[k][:2] != '\n' and contenu[k][:2] != '  ':
             recup.append(contenu[k])
 
     
     f1 = open(filename,"w")
     tmp = ''.join(recup)
-    #print (tmp)
+
     f1.write(tmp)
     f1.close()
 
@@ -95,20 +93,23 @@ def launch_simul_bis (n,theta,rep,pro=4):
     
         if not os.path.exists(path):
             os.mkdir(path)
-        liste = list()
+        dico = dict()
         for i in ['-e','-l']:
             for j in np.arange(0, 1.0, 0.1).tolist() : 
                 filename = os.path.join(path,"out"+"_"+str(i[1])+"_"+str(round(j,1))+".txt")
             
-                s = "./SimulTrees/SiteFrequencySpectrum "+str(n)+" "+str(theta)+" "+ str(rep) +" "+i+" "+str(j)+" " +"> "+filename
-                liste.append(s)
-        return liste
+                s = "./SimulTrees/SiteFrequencySpectrum "+str(n)+" "+str(theta)+" "+ str(rep) +" "+i+" "+str(j)+" -F" +"> "+filename
+                dico[filename]=(filename,s)
+        return dico,path
         
-    test = genere_liste(n,theta,rep)
+    test,path = genere_liste(n,theta,rep)
     pool = Pool(processes = pro)
-    results = pool.map(lancement, test.keys())
+    results = pool.map(lancement, test.values())
     
-"""    
+    return path
+    
+    
+ 
 
 def all_file_freq(path):
     all_file = glob.glob(os.path.join(path,'*'))
@@ -192,6 +193,21 @@ def meilleur_scena(data,all_freq):
     
 #recup_freq()
 #launch_simul('20','2','4')
+d_repli = {0.35: 36, 0.5: 12, 0.44999999999999996: 6, 0.25: 34, 0.175: 56, 0.325: 19, 0.375: 13, 0.025: 106, 0.425: 21, 0.05: 73, 0.19999999999999996: 4, 0.42500000000000004: 3, 0.275: 26, 0.15000000000000002: 2, 0.125: 22, 0.30000000000000004: 4, 0.22499999999999998: 2, 0.0: 237, 0.225: 55, 0.09999999999999998: 1, 0.475: 21, 0.07499999999999996: 3, 0.17500000000000004: 8, 0.15: 31, 0.1: 24, 0.4: 23, 0.32499999999999996: 4, 0.3: 29, 0.075: 32, 0.2: 51, 0.45: 21}
+
+
+mini = max(list(d_repli.values()))
+
+#fichier = launch_simul(100,mini,1000)
+
+fichier = (launch_simul_bis (100,mini,1000))
+d =all_file_freq(fichier)
+donne = d ['out_l_0.9']
+plt.plot(  [i for i in range (len(donne))],donne,'b*' )
+
+
+plt.plot(list(map(lambda x: x*100,list(d_repli.keys() ))), list(d_repli.values()),'c*' )
+#plt.plot(list(d_repli.keys()), list(d_repli.values()),'c*' )
 
 
 
@@ -202,20 +218,7 @@ def meilleur_scena(data,all_freq):
 
 #donnes_fr = {0.35: 22, 0.5: 12, 0.75: 3, 0.25: 34, 0.425: 21, 0.225: 55, 0.9: 1, 0.125: 20, 0.625: 8, 0.025: 106, 0.3: 29, 0.575: 3, 0.475: 12, 0.075: 32, 0.775: 2, 0.4: 17, 0.65: 14, 0.925: 3, 0.875: 2, 0.8: 4, 0.85: 2, 0.0: 237, 0.05: 73, 0.525: 9, 0.6: 6, 0.675: 4, 0.7: 4, 0.55: 6, 0.15: 31, 0.1: 24, 0.275: 26, 0.825: 8, 0.175: 56, 0.375: 13, 0.2: 51, 0.45: 21, 0.325: 19}
 
-d_repli = {0.35: 36, 0.5: 12, 0.44999999999999996: 6, 0.25: 34, 0.175: 56, 0.325: 19, 0.375: 13, 0.025: 106, 0.425: 21, 0.05: 73, 0.19999999999999996: 4, 0.42500000000000004: 3, 0.275: 26, 0.15000000000000002: 2, 0.125: 22, 0.30000000000000004: 4, 0.22499999999999998: 2, 0.0: 237, 0.225: 55, 0.09999999999999998: 1, 0.475: 21, 0.07499999999999996: 3, 0.17500000000000004: 8, 0.15: 31, 0.1: 24, 0.4: 23, 0.32499999999999996: 4, 0.3: 29, 0.075: 32, 0.2: 51, 0.45: 21}
-
-
-mini = max(list(d_repli.values()))
-
-fichier = launch_simul(100,mini,1000)
-d =all_file_freq(fichier)
-donne = d ['out_l_0.9']
-#plt.plot(  [i for i in range (len(donne))],donne,'b' )
-
-
-#plt.plot(list(map(lambda x: x*100,list(d_repli.keys() ))), list(d_repli.values()),'c*' )
-#plt.plot(list(d_repli.keys()), list(d_repli.values()),'c*' )
-plt.hist([donne,list(d_repli.values())])
+#plt.hist([donne,list(d_repli.values())])
 """
 description = \
     "Description:\n\n" + \
